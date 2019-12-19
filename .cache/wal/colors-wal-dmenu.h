2485eb241982788ab3391025ff9e2c8831e7cb69
ninewb <nick@noteshade.com>
2019-12-19 19:42:51 +0000

    ignores

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#dfe4ed", "#341f68" },
-	[SchemeSel] = { "#dfe4ed", "#4161B5" },
-	[SchemeOut] = { "#dfe4ed", "#A1B3DA" },
-};
